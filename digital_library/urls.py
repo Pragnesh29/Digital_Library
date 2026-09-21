1747df119de0c07792352df81c882d8dd01f0122
Pragnesh2920 <pagupatel29@gmail.com>
2026-09-21 21:45:19 +0530

    Add /SarvatraGyanKosh/ prefix to all application URLs and redirect root / to /SarvatraGyanKosh/

--- digital_library/urls.py
+++ digital_library/urls.py
@@ -18,11 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def root_redirect(request):
+    return redirect('/SarvatraGyanKosh/')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('library.urls')),
+    path('', root_redirect),
+    path('SarvatraGyanKosh/', include([
+        path('admin/', admin.site.urls),
+        path('accounts/', include('accounts.urls')),
+        path('', include('library.urls')),
+    ])),
 ]
 
 if settings.DEBUG:
